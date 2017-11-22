d7491fc21dc6bc96a01b85384a262d27f00883ff
Nacho Merino <escatman@gmail.com>
2017-11-22 11:19:27 +0100

    add bootsrtrap 4.0

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import $ from 'jquery';
-import './styles.css';
+import './styles.scss';
 
 /* eslint-disable */
 // eslint-disable-next-line or // eslint-disable-line You can also do this for one rule like // eslint-disable-next-line no-console (no-console is the name of the only rule to be turned off for the next line)
 /* eslint-enable */
 
 $(() => {
-  $('body').html('<h1>Hello F*^&ing World!</h1><h2>GrowME</h2><button id="press">HERE</button>');
-  $('#press').click(() => {
+  $('body').html('<h1>Hello F*^&ing World!</h1><h2>TransformME</h2><button id="grow">EatME</button><button id="shrink">EatME</button>');
+  $('#grow').click(() => {
     const div = $('h2');
-    div.animate({ fontSize: '3em', opacity: '0.4' }, 'slow');
+    div.animate({ fontSize: '3em' }, 'slow');
+  });
+  $('#shrink').click(() => {
+    const div = $('h2');
+    div.animate({ fontSize: '1em' }, 'slow');
   });
 });
