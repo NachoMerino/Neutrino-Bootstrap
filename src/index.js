1839371fcde012c6409df6008212acb8fe4c31fd
Nacho Merino <escatman@gmail.com>
2017-11-22 13:03:50 +0100

    navbar working

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
 import $ from 'jquery';
+import 'bootstrap/js/src';
 import './styles.scss';
 
 /* eslint-disable */
 // eslint-disable-next-line or // eslint-disable-line You can also do this for one rule like // eslint-disable-next-line no-console (no-console is the name of the only rule to be turned off for the next line)
 /* eslint-enable */
 
+const navbar = `
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>`;
+
 $(() => {
-  $('body').html('<h1>Hello F*^&ing World!</h1><h2>TransformME</h2><button id="grow">EatME</button><button id="shrink">EatME</button>');
-  $('#grow').click(() => {
-    const div = $('h2');
-    div.animate({ fontSize: '3em' }, 'slow');
-  });
-  $('#shrink').click(() => {
-    const div = $('h2');
-    div.animate({ fontSize: '1em' }, 'slow');
-  });
+  $('#root').append(navbar);
 });
