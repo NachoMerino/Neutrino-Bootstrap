d6b0be9d2a2e1a4abd7a29d3c90914d361b92b39
Nacho Merino <escatman@gmail.com>
2017-11-22 15:57:56 +0100

    added pictures

--- src/index.js
+++ src/index.js
@@ -1,48 +1,55 @@
 import $ from 'jquery';
 import 'bootstrap/js/src';
 import './styles.scss';
+import navbar from './templates/navbar.html';
 
 /* eslint-disable */
 // eslint-disable-next-line or // eslint-disable-line You can also do this for one rule like // eslint-disable-next-line no-console (no-console is the name of the only rule to be turned off for the next line)
 /* eslint-enable */
 
-const navbar = `
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+function loadPict(number) {
+  const pict = `../img/grid${number}.jpg`;
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+  const card = `
+<div class="card" >
+  <img class="card-img-top" src="${pict}" alt="Card image cap ${number}">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-</nav>`;
+</div>`;
+  return card;
+}
+
+const demoCard = `
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      ${loadPict(1)}
+    </div>
+    <div class="col-sm">
+      ${loadPict(2)}
+    </div>
+    <div class="col-sm">
+      ${loadPict(3)}
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm">
+      ${loadPict(4)}
+    </div>
+    <div class="col-sm">
+      ${loadPict(5)}
+    </div>
+    <div class="col-sm">
+      ${loadPict(6)}
+    </div>
+  </div>
+</div>`;
 
 $(() => {
-  $('#root').append(navbar);
+  $('#root')
+    .append(navbar)
+    .append(demoCard);
 });
