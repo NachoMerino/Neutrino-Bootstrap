6cad8739482ae9c0f3044fb89fa74a8d04561d08
Nacho Merino <escatman@gmail.com>
2017-11-29 10:54:58 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -2,132 +2,58 @@ import $ from 'jquery';
 import 'bootstrap/js/src';
 import './styles.scss';
 import navbar from './templates/navbar.html';
+import carousel from './templates/carousel.html';
 
-/* eslint-disable */
-// eslint-disable-next-line or // eslint-disable-line You can also do this for one rule like // eslint-disable-next-line no-console (no-console is the name of the only rule to be turned off for the next line)
-/* eslint-enable */
-function loadPict(nameOfImg) {
-  const pict = `../img/${nameOfImg}.jpg`;
-  return pict;
-}
+/**
+ * Carousel
+ */
 
-function loadCard(nameOfImg) {
-  const card = `
-<div class="card" >
-  <img class="card-img-top" src="${loadPict(nameOfImg)}" alt="Card image cap ${nameOfImg}">
+
+/**
+ * Products
+ */
+const pictures = [
+  'kitten-2.jpg',
+  'kitten-little.jpg',
+  'kitten.jpg',
+  'koala.jpg',
+  'pinguin.jpg',
+  'sloth.jpg',
+];
+
+function mkCard(img) {
+  return `
+<div class="card">
+  <img class="card-img-top" src="static/${img}" alt="Card image cap">
   <div class="card-body">
     <h4 class="card-title">Card title</h4>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
-</div>`;
-  return card;
+</div>
+`;
 }
 
-function loadCarrouselImg(nameOfImg) {
-  const carrouselImg = `
-    <div class="col-sm">
-      <img class="d-block" src="#" style="background-image: url(${loadPict(nameOfImg)});" alt="."/>
-    </div>
-  `;
-  return carrouselImg;
-}
-
-function loadCarrouselText(textSide) {
-  const carrouselText = `
-    <div class="col-sm carrouseltext ${textSide}">
-            <h1 class="display-3">Hello, world!</h1>
-            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            <hr class="my-4">
-            <p class="lead">
-              <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-            </p>
-          </div>`;
-  return carrouselText;
-}
-
-function loadCarrousel(nameOfImg, textSide) {
-  if (textSide === 'right') {
-    const carrouselCont = `
+const grid = `
 <div class="container-fluid">
-        <div class="row">
-          ${loadCarrouselImg(nameOfImg)}
-          ${loadCarrouselText(textSide)}
-        </div>
-      </div>`;
-    return carrouselCont;
-  }
-  const carrouselCont = `
-<div class="container-fluid">
-        <div class="row">
-          ${loadCarrouselText(textSide)}
-          ${loadCarrouselImg(nameOfImg)}
-        </div>
-      </div>`;
-  return carrouselCont;
-}
+  <div class="row shop-products-row">
+    <div class="col-12 col-lg-4">${mkCard(pictures[0])}</div>
+    <div class="col-12 col-lg-4">${mkCard(pictures[1])}</div>
+    <div class="col-12 col-lg-4">${mkCard(pictures[2])}</div>
+  </div>
+  <div class="row shop-products-row">
+    <div class="col-12 col-lg-4">${mkCard(pictures[3])}</div>
+    <div class="col-12 col-lg-4">${mkCard(pictures[4])}</div>
+    <div class="col-12 col-lg-4">${mkCard(pictures[5])}</div>
+  </div>
+</div>
+`;
 
-const carrousel = `
-<div id="carrousel" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carrousel" data-slide-to="0" class="active"></li>
-    <li data-target="#carrousel" data-slide-to="1"></li>
-    <li data-target="#carrousel" data-slide-to="2"></li>
-    <li data-target="#carrousel" data-slide-to="3"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      ${loadCarrousel('grid3', 'left')}
-    </div>
-    <div class="carousel-item">
-      ${loadCarrousel('grid6', 'right')}
-    </div>
-    <div class="carousel-item">
-      ${loadCarrousel('admiral-ackbar', 'left')}
-    </div>
-    <div class="carousel-item">
-      ${loadCarrousel('grid5', 'right')}
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carrousel" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carrousel" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>`;
-
-const demoCard = `
-<div id="cards" class="container">
-  <div class="row">
-    <div class="col-sm">
-      ${loadCard('grid1')}
-    </div>
-    <div class="col-sm">
-      ${loadCard('grid2')}
-    </div>
-    <div class="col-sm">
-      ${loadCard('grid3')}
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-sm">
-      ${loadCard('grid4')}
-    </div>
-    <div class="col-sm">
-      ${loadCard('grid5')}
-    </div>
-    <div class="col-sm">
-      ${loadCard('grid6')}
-    </div>
-  </div>
-</div>`;
 
 $(() => {
   $('#root')
     .append(navbar)
-    .append(carrousel)
-    .append(demoCard);
+    .append(carousel)
+    .append(grid);
+  $('.carousel').carousel();
 });
