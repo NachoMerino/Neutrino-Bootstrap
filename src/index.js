b438529ce23b804905ea1c6582db315b44699ab6
Nacho Merino <escatman@gmail.com>
2017-11-29 15:09:03 +0100

    add the categories to the navbar

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import navbarTemplate from './templates/navbar.html';
 import mkCarousel from './carousel';
 
 $(() => {
+  $('#root').append(navbarTemplate);
   $.ajax('./static/categories.json')
     .done((categories) => {
       const $carousel = mkCarousel(categories);
       $('#root')
         .append($carousel);
       $carousel.carousel();
+      $('.navbar-nav').empty();
+      for (let i = 0; i < categories.length; i += 1) {
+        $('.navbar-nav').append(`
+        <li class="nav-item">
+        <a class="nav-link" href="#">${categories[i].name}<span class="sr-only"></span></a>
+      </li>`);
+      }
     });
-  $('#root').append(navbarTemplate);
 });
